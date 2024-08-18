f37ac17c26d5331e2654f159b038b047146e189e
Vivaan jain <vivaanjaindps@gmail.com>
2024-08-18 08:40:24 +0530

    my first commit

---
+++ c++.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){         
+//  int number {-300};//signed is used for assigning negative value to a variable the possibility of numbers is reduced to half when we use signed 
+ //unsigned is for postive integers
+//  cout <<sizeof( short int);
+//The short keyword modifies the minimum values that a data type can hold
+//The long keyword modifies the maximum values that a data type can hold. It is used for large integers that are in the range of -2147483647 to 2147483647
+//there can be combination such as signed long int unsigned short int 
+//short-2bytes long -8 bytes int-4bytes signed-for both + and negative storage
+// long long var{879};
+// cout<<sizeof(var);
+//fractional numbers are represented as float 
+// in float modifier is double that has 8 bytes 
+// and long double 12 bytes
+//float can represent numbers upto precision of 7 decimal double 15 
+// long double num (1.23e-1);//we can represent floats with 10 ki power --e8-- means 10 ki power 8
+// cout<<num/0<<endl;//float me divide by 0 par infinity aajata hai
+
+// cout<<num;
+// cout<<0.0/0.0<<endl;//gives nan
+// long double in (1.234567789999);
+
+// cout<<in;
+/*int age;
+cout<<"what is you age?"<<endl;
+cin>>age;
+ if (age<=18){
+    cout<<" you are ineligible";
+ }
+ else{
+    cout<<" you are eligible";
+ }
+ */
+//booleans occupy 8 bytes
+}
